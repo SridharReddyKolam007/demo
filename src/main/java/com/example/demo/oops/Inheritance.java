640c175ac8b67d0aff8d85c4382a4744485488c6
sridhar <sridharreddykolam007@gmail>
2025-03-26 23:07:41 -0500

    test commit

--- src/main/java/com/example/demo/oops/Inheritance.java
+++ src/main/java/com/example/demo/oops/Inheritance.java
@@ -3,5 +3,6 @@ package com.example.demo.oops;
 public class Inheritance {
     public static void main(String[] args) {
         System.out.println("my name is sridhar ");
+        System.out.println("my name is dinesh");
     }
 }
